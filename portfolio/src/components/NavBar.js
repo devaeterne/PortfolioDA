ca087f82fa5b08d61a8f14c7ccb057365c408a43
devaeterne <info@safaksariyildiz.com>
2024-02-16 13:29:21 +0300

    sss

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,57 +1,66 @@
-import { Navbar, Container } from 'react-bootstrap';
-import { useState } from 'react';
-import logo from '../assets/logo.svg';
-import navIcon1 from '../assets/navIcon1.svg';
-import navIcon2 from '../assets/navIcon2.svg';
-import navIcon3 from '../assets/navIcon3.svg';
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
-export const Navbar = () => {
-    const [activeLink, setActiveLink] = useState('Home');
-    const [scalled, seScrolled] = useState('false');
+export const NavBar = () => {
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                seScrolled(true);
-            } else {
-                seScrolled(false);
-            }
-        }
-        window.addEventListener('scroll', onScroll);
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
 
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
-
-    const onUptdateActiveLink = (value) => {
-        setActiveLink(value);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     }
-    return (
-        <Navbar expand="lg" className={scrolled ? "scrolled":""}>
-            <Container>
-                <Navbar.Brand href="#home">DevAeterne
-                    <img src={'pp.png'} alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className='navbar-toggler-icon'></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/" className={activeLink === 'home' ? 'active navbar-link':'navbar-link'} onClick={() => onUptdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="/about"className={activeLink === 'about' ? 'active navbar-link':'navbar-link'}onClick={() => onUptdateActiveLink('about')}>About Me</Nav.Link>
-                        <Nav.Link href="/projects"className={activeLink === 'projects' ? 'active navbar-link':'navbar-link'}onClick={() => onUptdateActiveLink('projects')}>Projects</Nav.Link>
-                        <Nav.Link href="/articles"className={activeLink === 'articles' ? 'active navbar-link':'navbar-link'}onClick={() => onUptdateActiveLink('articles')}>Articles</Nav.Link>
-                    </Nav>
-                    <span className='navbar-text'>
-                        <div className='social-icon'>
-                            <a href='#'><img src={'navIcon1'} alt="Github"/></a>
-                            <a href='#'><img src={'navIcon2'} alt="Instagram"/></a>
-                            <a href='#'><img src={'navIcon3'} alt="Medium"/></a>
-                            <a href='#'><img src={'navIcon4'} alt="Linkedin"/></a>
-                        </div>
-                        <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
+              </div>
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Let’s Connect</span></button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
 }
